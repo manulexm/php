74e8f585248d252d66f1045b165180f52d95e074
manulexm <emanuelly.lara.ferreira@escola.pr.gov.br>
2025-09-16 12:23:54 -0300

    Add files via upload

---
+++ manu 1d/page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form class = "manu">
+<h3>cadastro</h3>
+<input txpe ="text" placehocder = "login"id ="login">
+<input type ="passworo"placeholder ="senha"id="senha">
+<input type ="tel"placeholder="telefone"id="telefone">
+<input type ="email"placehocder="email"id="email">
+  <input type ="submit"onclick ="logar"  (),retuirn false>
+ </form>   
+</body>
+</html>
